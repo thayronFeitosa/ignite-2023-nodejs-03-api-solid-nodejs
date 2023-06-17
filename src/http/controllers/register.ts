6dff0a00cffcad25151ff2542a63ced65faf04ba
thayron matos <thayronf3@gmail.com>
2023-06-17 07:43:21 -0300

    feat: Interface do repositorio

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,4 +1,4 @@
-import { PrismaUsersRepository } from '@/repositories/prisma-useres-repository'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-useres-repository'
 import { RegisterUseCase } from '@/use-cases/register'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
